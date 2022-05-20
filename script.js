7f7c613b1f6b005db97a41f19f23e8fbe0c196eb
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-05-21 02:39:38 +0530

    buttons add

--- script.js
+++ script.js
@@ -4,10 +4,12 @@ Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
         if(e.target.innerHTML == '='){
             String = eval(String);
-            document.querySelector('input').value = String;
+           document.querySelector('input').value = String;
         }
+        else{
         console.log(e.target)
         String = String + e.target.innerHTML;
         document.querySelector('input').value = String;
+        }
     })
 })
