1d27bf8996bbd0f4eaa17a9625c309e4d1697222
Anshul <81064196+Anshul9602@users.noreply.github.com>
2022-05-21 02:28:55 +0530

    evaluation operation use to get value

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ let  String ="";
 let buttons =document.querySelectorAll('.button');
 Array.from(buttons).forEach((button)=>{
     button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML == '='){
+            String = eval(String);
+            document.querySelector('input').value = String;
+        }
         console.log(e.target)
         String = String + e.target.innerHTML;
         document.querySelector('input').value = String;
